e7697aeb2b05dd1b577ea4127de6db089115c876
Aleksei Ivanyuk <ivanyuk_aleksei@mail.ru>
2022-03-16 10:54:46 +0300

    Commit

---
+++ src/Calculator.java
@@ -0,0 +1,26 @@
+import java.util.function.*;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y != 0) {
+            return x / y;
+        } else return 0;
+    };
+    //TODO Если y равен 0, то возникает математическая ошибка,
+    // соответственно добавим проверку
+    // на условие равенства 0, и вернем 0, если равен.
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+
+}
